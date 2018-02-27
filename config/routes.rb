3e39fb3393259d182fcee8a7fd705dc60258d008
pumpkin <zhenhuanlee@outlook.com>
2018-02-27 15:08:26 +0800

    demo

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :people
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "welcome/say_hello" => "welcome#say"
+
+  get "welcome" => "welcome#index"
+
+  root 'welcome#index'
+end
